080381ae8ed98e2a34830c18257c100953d11d04
benjaav <138171252+benjaav@users.noreply.github.com>
2025-04-18 05:14:35 -0400

    ya

--- ecommerce-frontend/src/index.js
+++ ecommerce-frontend/src/index.js
@@ -1,4 +1,3 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-// Apunta al dominio y prefijo común de tu API
+// apuntas a https://api.codestorebl.com/api/
+// dejamos la base apuntando al prefijo /api/
 axios.defaults.baseURL = 'https://api.codestorebl.com/api/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
