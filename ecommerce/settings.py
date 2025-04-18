09b7e47b6d2878f458d37e9c6ddb2788cc67bf8d
benjaav <138171252+benjaav@users.noreply.github.com>
2025-04-18 15:03:47 -0400

    bdss

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,13 +134,14 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = {
+DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
