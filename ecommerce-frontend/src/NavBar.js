842a7559680a8aea8994c142ce8c0e6d59d0f6fb
benjaav <138171252+benjaav@users.noreply.github.com>
2025-04-19 01:10:17 -0400

    navbarr

--- ecommerce-frontend/src/NavBar.js
+++ ecommerce-frontend/src/NavBar.js
@@ -18,12 +18,18 @@ function NavBar() {
       })
       .catch(error => {
         console.error("Error obteniendo usuario:", error);
+        if (error.response && error.response.status === 401) {
+          // Token inválido o expirado, forzar logout
+          localStorage.removeItem('accessToken');
+          setUsername(null);
+          navigate('/login');
+        }
       });
+    } else {
+      setUsername(null);
     }
-  }, [token]);
+  }, [token, navigate]);
 
-
-  
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     setUsername(null);
@@ -53,14 +59,16 @@ function NavBar() {
           </button>
         </Link>
 
-        <Link to="/login" className="link-btn">
-          <button className="button">
-            <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z" />
-            </svg>
-            <span className="btn-text">Login</span>
-          </button>
-        </Link>
+        {!username && (
+          <Link to="/login" className="link-btn">
+            <button className="button">
+              <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z" />
+              </svg>
+              <span className="btn-text">Login</span>
+            </button>
+          </Link>
+        )}
 
         <Link to="/cart" className="link-btn">
           <button className="button">
