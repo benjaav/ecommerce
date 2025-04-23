bbd7aef9fd2fab381a86022013045f3cb18013a2
benjaav <138171252+benjaav@users.noreply.github.com>
2025-04-23 11:20:34 -0400

    appss

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
-import ProductDetail from './ProductDetail'; // <-- importar el nuevo componente
+import ProductDetail from './ProductDetail';
 import Login from './Login';
 import Register from './Register';
 import Cart from './Cart';
@@ -15,22 +15,19 @@ function App() {
   return (
     <Router>
       <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/products" element={<Products />} />
-  <Route path="/products/:id" element={<ProductDetail />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/cart" element={
-    <ProtectedRoute>
-      <Cart />
-    </ProtectedRoute>
-  } />
-  <Route path="/checkout" element={
-    <ProtectedRoute>
-      <Checkout />
-    </ProtectedRoute>
-  } />
-</Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Productos accesibles sin login */}
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Carrito y checkout accesibles sin login */}
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </Router>
   );
 }
