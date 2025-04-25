98d01c0f54dff89ee3212010dcc47b8cde358790
benjaav <138171252+benjaav@users.noreply.github.com>
2025-04-25 17:26:48 -0400

    cga

--- ecommerce-frontend/src/Home.js
+++ ecommerce-frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
-import styled from 'styled-components';
 import './HeroAnimation.css';
 
 function Home() {
@@ -19,9 +18,7 @@ function Home() {
         <span aria-hidden="true" className="hover-text">&nbsp;CodeStore&nbsp;</span>
       </button>
 
-      {/* Sección hero */}
       <section className="home-hero">
-        {/* Contenedor para colocar el botón Iniciar Sesión y el botón Invitado arriba a la derecha */}
         <div className="hero-header">
           <Link to="/login" className="login-btn">Iniciar Sesión</Link>
           <button onClick={handleGuestLogin} className="guest-btn">Ingresar como invitado</button>
@@ -29,10 +26,7 @@ function Home() {
 
         <h1>¡Bienvenido a CodeStore!</h1>
         <p>Explora nuestros productos y descubre ofertas increíbles.</p>
-
-        <div className="home-hero-buttons">
-          <Link to="/products" className="cta-button">Ver Productos</Link>
-        </div>
+        {/* El CTA "Ver Productos" está oculto vía CSS */}
       </section>
     </>
   );
